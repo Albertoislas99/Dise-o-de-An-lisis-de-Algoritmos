8818304ed15d127ff87aabad6937e87d2db3710b
Albertoislas99 <120591093+Albertoislas99@users.noreply.github.com>
2022-12-14 20:08:51 -0600

    Update FuerzaBruta.cpp

--- FuerzaBruta.cpp
+++ FuerzaBruta.cpp
@@ -1,1 +1,46 @@
 #include<iostream>
+#include<string.h>
+#define maxTexto 400
+#define maxPatron 400
+
+
+using namespace std;
+
+void FuerzaBruta(char[], char[], int, int);
+int main(void)
+{
+      char Patron[maxPatron];
+      char Texto[maxTexto];
+      int x, y;
+      cout << "Ingresa el Texto:";
+      cin>>(Texto);
+      x = strlen(Texto);
+      do {
+        
+                 cout <<"\nIngresa el Patron:";
+                 cin >> Patron;
+                 y = strlen(Patron);
+      } while (y>x);
+      
+      FuerzaBruta(Texto, Patron, x, y);
+}
+
+void FuerzaBruta(char texto[], char patron[], int x, int y)
+{
+       int i,j,k, cont=0;
+       char temp[100];
+       for (i = 0; i <= x; i++)
+       {
+         
+             for (j = i, k = 0; j<y; j++, k++)
+                  temp[k] = Texto[i + k];
+             temp[k] = '\0';
+             if (strcmp(Patron, temp) == 0)
+             {
+                  cout << "\nLa posision es : "<< i << "\n";
+                  cont++;
+             }
+             y++;
+       }
+       cout << "\n\nLas inetraciones que se hicieron fueron :"<< cont << end1;
+}
